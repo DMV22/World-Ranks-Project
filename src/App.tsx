1228f3eafa96d3121471d85a73a0e0ef20593285
DMV22 <maksdivnich12@gmail.com>
2026-06-03 23:45:33 +0300

    refactor: restructure App composition and migrate layout to CSS Modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
+import { useMemo } from "react";
 import Header from "@/components/header";
-import Main from "@/components/country-list";
+import CountryList from "@/components/country-list";
+import FiltersSidebar from "./components/filters-sidebar";
 import Footer from "@/components/footer";
-import { useCountries } from "@/hooks/use-country";
-import "./index.css";
 
+import { useCountries } from "@/hooks/use-country";
+import useCountryFilters from "@/hooks/use-country-filters";
+
+import { filterCountries } from "@/utils/filter-countries";
+
+import styles from "./App.module.css";
 
 function App() {
   const { countries, isLoading, isError } = useCountries();
+  const { filters, updateFilter } = useCountryFilters();
+
+  const filteredCountries = useMemo(() => filterCountries(countries, filters),
+    [countries, filters]
+  );
 
   return (
-    <div className="App">
+    <div className={styles.page}>
       <Header />
-      <Main data={countries} isLoading={isLoading} isError={isError} />
+
+      <main className={styles.contentShell}>
+        <section className={styles.panel}>
+          <div className={styles.layout}>
+            <FiltersSidebar
+              filters={filters}
+              updateFilter={updateFilter}
+            />
+
+            <div className={styles.mainContent}>
+              <CountryList
+                data={filteredCountries}
+                isLoading={isLoading}
+                isError={isError}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+
       <Footer />
     </div>
   );

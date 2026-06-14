651d0cb90e8b82ba0baf5503de757dbfea595be0
DMV22 <maksdivnich12@gmail.com>
2026-06-14 23:59:58 +0300

    refactor: extend country model for details page data

--- src/interfaces/country.ts
+++ src/interfaces/country.ts
@@ -9,10 +9,22 @@ export interface Country {
   };
   name: {
     common: string;
+    official: string;
   };
   population: number;
   area?: number;
   region: Region;
+  subregion?: string;
+  capital?: string[];
+  continents: string[];
+  borders?: string[];
+  languages?: Record<string, string>;
+  currencies?: Record<string,
+    {
+      name: string;
+      symbol?: string;
+    }
+  >;
   unMember: boolean;
   independent?: boolean;
 }

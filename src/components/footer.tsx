2e3268a1c38bb0319bab299ca49fa7f552513531
DMV22 <maksdivnich12@gmail.com>
2026-06-15 19:24:43 +0300

    feat: update Footer component and add dedicated footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,23 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
   return (
-    <footer className="footer">
-      {/* Footer content goes here */}
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <span className={styles.footerCopy}>
+          &copy; {new Date().getFullYear()} Country Finder. All rights reserved.
+        </span>
+
+        <div className={styles.footerLinks}>
+          <span>
+            Powered by <a href="https://restcountries.com" target="_blank" rel="noreferrer" className={styles.footerLink}>REST Countries API</a>
+          </span>
+          <span className={styles.footerDivider}>|</span>
+          <span>
+            Developed by <a href="https://github.com/DMV22" target="_blank" rel="noreferrer" className={styles.footerLink}>Max Dyvnych</a>
+          </span>
+        </div>
+      </div>
     </footer>
   );
 }
